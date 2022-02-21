65ad871f915443bc0f84f9dd50415f1a3c0684dc
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2022-02-22 01:05:05 +0600

    add postgresql

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -14,15 +14,11 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
